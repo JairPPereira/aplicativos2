f46aa67eca9e9a8f07d7a089cba5f12dc89e060d
JairPPereira <jairppereira21@gmail.com>
2021-11-02 13:38:20 -0300

    atualização

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -6,9 +6,15 @@ export default ({title, items}) => {
         <div>
            <h2>{title}</h2>
            <div className="MovieRow--listarea">
-               {items.results.length > 0 && items.results.map((item, key)=>(
-              <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
+              <div className="movieRow--list">
+              {items.results.length > 0 && items.results.map((item, key)=>(
+              <div className="movieRow--item">
+                  <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+              </div>
+              
                ))}
+              </div>
+
            </div>
         </div>
     )
